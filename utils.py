41e2afc53748a3fea8d4eb69790a740f6df39feb
xavier <xavier@debian.localdomain>
2015-02-17 16:42:58 +0100

    Repo refactoring

--- utils.py
+++
@@ -1,8 +0,0 @@
-from Tkinter import *
-
-def subimage(src, coord):
-    dst = PhotoImage()
-    dst.tk.call(dst, 'copy', src, '-from', 
-                coord[0], coord[1], coord[2], coord[3], 
-                '-to', 0, 0)
-    return dst
