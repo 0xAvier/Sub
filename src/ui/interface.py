e3773fb88b0c715f81a0138c4018ad63de26c2d2
BobCitron <cosmita@hotmail.fr>
2015-02-17 17:52:04 +0100

    Encapsulation of cards image getters + suppression of cards on click

--- src/ui/interface.py
+++ src/ui/interface.py
@@ -5,45 +5,38 @@ from cards_image import *
 class UIHand:
     # number of cards in the hand
     # must be 8 for the Coinche
-    nb_cards = 8
+    max_cards = 8
 
     # define the first card column
     def return_first_card_column(self):
-        column = [1, 1 + self.nb_cards, 1, 0]
+        column = [2, 2 + self.max_cards, 2, 0]
         # return the position
         return column[self.position]
-
+    
     # define the first card row
     def return_first_card_row(self):
-        row = [0, 1, 1 + self.nb_cards, 1]
+        row = [0, 1, 1 + self.max_cards, 1]
         # return the position
         return row[self.position]
 
-    # init the cards with randomness
-    def get_random_cards(self):
-        for i in xrange(0, self.nb_cards):
-            self.cards_coord[i] = get_random_card_coord()
-            
-    #
-    def remove_card(self):
-        print("Not implemented")
-    
+    def _remove_last_button(self):
+        self.buttons[len(self.buttons) - 1].destroy()
+        self.buttons.pop()
+
     #
     def play_card(self):
-        print("Not implemented")
+        self._remove_last_button()
 
     # initialize the button for the cards
     def init_cards_button(self):
         # defines if the hand must be displayed vertically
         # or horizonthally 
         vertical = [0, 1, 0, 1]
-
-        cards = Cards_image.get_cards_image_instance()
-        for i in xrange(0, self.nb_cards):
-            self.cards_image[i] = subimage(cards, self.cards_coord[i])
+        for i in xrange(0, self.max_cards):
+            self.cards_image[i] = UICards.get_random_card()
             # define the button
             self.buttons[i] = Button(self.frame, image=self.cards_image[i], 
-                                     command=self.frame.quit)
+                                     command=self.play_card)
             # which row
             row = self.return_first_card_row() 
 
@@ -51,7 +44,6 @@ class UIHand:
             column = self.return_first_card_column() 
             # display vertically or horizontally ?
             if vertical[self.position]:
-                
                 row += i / 2 * vertical[self.position]            
                 if not i % 2:
                     # side by side with the previous
@@ -66,19 +58,19 @@ class UIHand:
     def __init__(self, frame, position):
         self.frame = frame
         self.position = position
-        # init the cards
-        # list for the placement of the cards
-        self.cards_coord = [None]*self.nb_cards
         # list for recording the images itself
-        self.cards_image = [None]*self.nb_cards
+        self.cards_image = [None]*self.max_cards
         # place corresponding buttons
-        self.buttons = [None]*self.nb_cards
-        
-        # give random cards
-        self.get_random_cards()
+        self.buttons = [None]*self.max_cards
+
+        # add random card
+        for i in xrange(0, UIHand.max_cards):
+            self.cards_image[i] = UICards.get_random_card()
+
         # init button
         self.init_cards_button()
-        
+
+
 class App:
     def __init__(self, master):
         frame = Frame(master)
@@ -89,8 +81,11 @@ class App:
                       UIHand(frame, 2),
                       UIHand(frame, 3)]
 
+        self.quit = Button(frame, text = "Quit",
+                                     command=frame.quit)
+        self.quit.grid(row = 7, column = 13)
+        
 root = Tk()
-
 app = App(root)
 
 root.mainloop()
