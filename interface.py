310aff97d833143b85bd1e3b18766bdede1dd112
BobCitron <cosmita@hotmail.fr>
2015-04-14 14:34:30 +0200

    .gitignore is now working

--- interface.py
+++ interface.py
@@ -24,6 +24,14 @@ class HandDisplay:
         for i in xrange(0, self.nb_cards):
             self.cards_coord[i] = get_random_card_coord()
             
+    #
+    def remove_card(self):
+        print("Not implemented")
+    
+    #
+    def play_card(self):
+        print("Not implemented")
+
     # initialize the button for the cards
     def init_cards_button(self):
         # defines if the hand must be displayed vertically
@@ -32,68 +40,54 @@ class HandDisplay:
 
         cards = Cards_image.get_cards_image_instance()
         for i in xrange(0, self.nb_cards):
-            print(i)
-            card = subimage(cards, self.cards_coord[i])
+            self.cards_image[i] = subimage(cards, self.cards_coord[i])
             # define the button
-            self.buttons[i] = Button(self.frame, image=card, 
+            self.buttons[i] = Button(self.frame, image=self.cards_image[i], 
                                      command=self.frame.quit)
             # which row
             row = self.return_first_card_row() 
-            row += i * vertical[self.position]
+
             # which column
             column = self.return_first_card_column() 
-            column +=i * vertical[self.position]
+            # display vertically or horizontally ?
+            if vertical[self.position]:
+                
+                row += i / 2 * vertical[self.position]            
+                if not i % 2:
+                    # side by side with the previous
+                    column += 1
+            else:
+                # horizontally
+                column += i
             # place the button
             self.buttons[i].grid(row = row, column = column)
             
     # constructor
     def __init__(self, frame, position):
         self.frame = frame
-        #self.position = position
+        self.position = position
         # init the cards
-        #self.cards_coord = [None]*self.nb_cards
-        # must be modified (non-random cards must be given)
-        #self.get_random_cards()
+        # list for the placement of the cards
+        self.cards_coord = [None]*self.nb_cards
+        # list for recording the images itself
+        self.cards_image = [None]*self.nb_cards
         # place corresponding buttons
-        #self.buttons = [None]*self.nb_cards
-        #self.init_cards_button()
-        # local test
-        cards = Cards_image.get_cards_image_instance()
-        self.card = subimage(cards, get_random_card_coord())
-        self.hands_north = Button(frame, image=self.card,
-                                  command=frame.quit)
-        self.hands_north.grid(row = 0, column = 1)
+        self.buttons = [None]*self.nb_cards
+        
+        # give random cards
+        self.get_random_cards()
+        # init button
+        self.init_cards_button()
         
 class App:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
 
-        self.cards = Cards_image.get_cards_image_instance()
-        if self.cards == 0:
-            frame.quit()
-
-        self.hands = HandDisplay(frame, 0)
-
-        #self.card_north = subimage(self.cards, get_random_card_coord())
-        #self.hands_north = Button(frame, image=self.card_north, 
-        #                          command=frame.quit)
-        #self.hands_north.grid(row = 0, column = 1)
-
-        self.card_east = subimage(self.cards, get_random_card_coord())
-        self.hands_east = Button(frame, image=self.card_east, 
-                                 command=frame.quit)
-        self.hands_east.grid(row = 1, column = 2)
-
-        self.card_south = subimage(self.cards, get_random_card_coord())
-        self.hands_south = Button(frame, image=self.card_south, 
-                                  command=frame.quit)
-        self.hands_south.grid(row = 2, column = 1)
-
-        self.card_west = subimage(self.cards, get_random_card_coord())
-        self.hands_west = Button(frame, image=self.card_west, 
-                                 command=frame.quit)
-        self.hands_west.grid(row = 1, column = 0)
+        self.handsNorth = HandDisplay(frame, 0)
+        self.handsEast = HandDisplay(frame, 1)
+        self.handsSouth = HandDisplay(frame, 2)
+        self.handsWest = HandDisplay(frame, 3)
 
 root = Tk()
 
