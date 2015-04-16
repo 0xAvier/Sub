98a98052faf5a6c532fa8f82c8650f8bdc62603f
Franck de Goër <degoerdf@imag.fr>
2015-04-16 11:01:21 +0200

    Test for indentation with expand tabs

--- src/event/event_engine.py
+++ src/event/event_engine.py
@@ -8,50 +8,50 @@ EVT_CARD_PLAYED = 3
 class EventEngine(object):
 
 
-	def __init__(self, game):
-		self.game = game
-		# Define the function of the event manager that the 
-		# game engine should call at each new round
-		self.game.set_method(EVT_NEW_ROUND, self.new_round)
-		self.game.set_method(EVT_NEW_HAND, self.new_hand)
-		self.game.set_method(EVT_CARD_PLAYED, self.card_played)
-		self.ui = list()
+    def __init__(self, game):
+        self.game = game
+        # Define the function of the event manager that the 
+        # game engine should call at each new round
+        self.game.set_method(EVT_NEW_ROUND, self.new_round)
+        self.game.set_method(EVT_NEW_HAND, self.new_hand)
+        self.game.set_method(EVT_CARD_PLAYED, self.card_played)
+        self.ui = list()
 
 
-	def add_ui(self, ui, p = None):
-		"""
-			Add a user interface to the list of interfaces
-			to whom events must be notified
-			p is a list of players that play through this interface
+    def add_ui(self, ui, p = None):
+        """
+            Add a user interface to the list of interfaces
+            to whom events must be notified
+            p is a list of players that play through this interface
 
-		"""
-		if ui not in [ui[0] for ui in self.ui]:
-			self.ui.append((ui, p))
-		for player in p:
-			self.game.players[player].set_method(EVT_UI_GET_CARD, ui.get_card)
-
-
-	def new_round(self):
-		"""
-			Notify all interfaces that a new round has begun
-
-		"""
-		for ui in self.ui:
-			ui[0].new_round()
+        """
+        if ui not in [ui[0] for ui in self.ui]:
+            self.ui.append((ui, p))
+        for player in p:
+            self.game.players[player].set_method(EVT_UI_GET_CARD, ui.get_card)
 
 
-	def new_hand(self, p, h):
-		"""
-			Notify interfaces that a new hand has beend given to player p
-			@param p player concerned by the hand
-			@param h new hand for player p
+    def new_round(self):
+        """
+            Notify all interfaces that a new round has begun
 
-		"""
-		for ui in self.ui:
-			if p in ui[1]:
-				ui[0].new_hand(p, h)
+        """
+        for ui in self.ui:
+            ui[0].new_round()
 
 
-	def card_played(self, p, c):
-		for ui in self.ui:
-			ui.card_played(p, c)
+    def new_hand(self, p, h):
+        """
+            Notify interfaces that a new hand has beend given to player p
+            @param p player concerned by the hand
+            @param h new hand for player p
+
+        """
+        for ui in self.ui:
+            if p in ui[1]:
+                ui[0].new_hand(p, h)
+
+
+    def card_played(self, p, c):
+        for ui in self.ui:
+            ui.card_played(p, c)
