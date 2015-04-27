c7a9547edbfdcb43fcce780605cab97baca61054
Franck de Goër <degoerdf@imag.fr>
2015-04-27 15:22:17 +0200

    Add UI adapter

---
+++ src/adapter/i_ui_adapter.py
@@ -0,0 +1,47 @@
+#-*- coding: utf-8 -*-
+
+from abc import ABCMeta
+
+class IUIAdapter(object):
+
+    __metaclass__ = ABCMeta
+
+
+    def __init__(self):
+        pass
+
+
+    def card_played(self, p, c):
+        raise NotImplemented
+
+    
+    def new_bid(self, b):
+        raise NotImplemented
+
+
+    def new_round(self): 
+        raise NotImplemented
+
+
+    def new_deal(self): 
+        raise NotImplemented
+
+
+    def end_of_trick(self, p): 
+        raise NotImplemented
+
+
+    def end_bidding(self):
+        raise NotImplemented
+
+
+    def new_hand(self):
+        raise NotImplemented
+
+
+    def update_score(self, s): 
+        raise NotImplemented
+
+
+    def get_consoles(self):
+        raise NotImplemented
