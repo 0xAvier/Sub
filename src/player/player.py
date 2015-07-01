6c517db48fd1f5bdac3433fb7e2d2afce90b62fe
BobCitron <cosmita@hotmail.fr>
2015-07-01 12:37:50 +0200

    Coding style modification

--- src/player/player.py
+++ src/player/player.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+#-*- coding: utf-8 -*-
 
 class Player(IPlayer):
     """
@@ -6,10 +6,11 @@ class Player(IPlayer):
     """
 
 
-    def __init__(self, pid):
+    def __init__(self, pid, is_removable = false):
         self.pid = pid
         self._player_mind = None 
         self._player_renders = [] 
+        self._is_removable = is_removable
 
 
     def set_mind(self, mind):
@@ -21,52 +22,51 @@ class Player(IPlayer):
 
 
     def give_cards(self, cards):
-        # both render and mind 
+        # Both render and mind 
         self._player_mind.give_cards(self, cards)
         for render in self._player_renders:
             render.give_cards(self, cards)
 
 
     def team(self):
-        # do not depend from render nor mind 
+        # Do not depend from render nor mind 
         return self.id % 2
 
 
     def get_card(self, played, playable):
-        # pure mind
+        # Pure mind
         self._player_mind.get_card(self, played, playable)
 
 
     def get_coinche(self):
-        # pure mind
+        # Pure mind
         self._player_mind.get_coinche()
 
 
     def get_bid(self, bidded, biddable):
-        # pure mind 
+        # Pure mind 
         self._player_mind.get_bid(bidded, biddable)
 
 
     def bidded(self, bid):
-        # both render and mind
+        # Both render and mind
         self._player_mind.bidded(bid)
         for render in self._player_renders:
             render.bidded(bid)
 
 
     def played(self, pid, card):
-        # both render and mind
+        # Both render and mind
         self._player_mind.played(pid, card)
         for render in self._player_renders:
             render.played(pid, card)
 
 
     def is_removable(self):
-        # can be rewritten for the AI
-        # TODO abstract method ? Or in mind ?
-        return false;
+        return self._is_removable
 
 
     def reset_hand(self):
-        # pure mind
+        # Pure mind
         self._player_mind.reset_hand()
+
