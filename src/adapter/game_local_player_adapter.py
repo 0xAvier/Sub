11357c3ef9c5a830904eeb9acc6287927dd0091d
BobCitron <cosmita@hotmail.fr>
2015-07-03 13:51:35 +0200

    Remove game from game_local_adapter

--- src/adapter/game_local_player_adapter.py
+++ src/adapter/game_local_player_adapter.py
@@ -11,8 +11,7 @@ from src.game.round import BID_COINCHE, BID_BIDDING
 class GameLocalPlayerAdapter(IGameAdapter, IPlayerAdapter):
 
 
-    def __init__(self, game, player):
-        self.game = game
+    def __init__(self, player):
         self.player = player
         self.player.set_method(COINCHE_CODE, self.coinche)
         self.__coinched = False
@@ -56,10 +55,6 @@ class GameLocalPlayerAdapter(IGameAdapter, IPlayerAdapter):
 
     def is_removable(self):
         return self.player.is_removable()
-
-
-    def join(self):
-        return self.game.add_player(self)
 
     
     def reset_hand(self):
